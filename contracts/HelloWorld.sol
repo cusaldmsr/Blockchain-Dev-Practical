48bd343a2e63da3a7fdfb4d2fa3a5578258a1383
cusaldmsr <kusal2002d@Gmail.com>
2025-06-28 21:46:09 +0530

    Removed public access modifier of greeting variable

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.19;
 contract HelloWorld {
-    string public greeting = "Hello World!";
+    string greeting = "Hello World!";
 
     function hello() public view returns (string memory) {
         return greeting;
