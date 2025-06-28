460c900bb812c0ee0adbfcc9509d76f80008d521
cusaldmsr <kusal2002d@Gmail.com>
2025-06-28 22:11:44 +0530

    Update sum methods

--- contracts/Sum.sol
+++ contracts/Sum.sol
@@ -5,7 +5,7 @@ contract Sum {
     uint256 a;
     uint256 b;
 
-    function sum(uint256 _a, uint256 _b) external pure returns (uint256) {
+    function sum(uint256 _a, uint256 _b) public pure returns (uint256) {
         return _a + _b;
     }
     function setSum(uint256 _a, uint256 _b) public {
