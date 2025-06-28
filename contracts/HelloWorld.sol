5d9806f9851d084d7f90210bff6cd0426428dea1
cusaldmsr <kusal2002d@Gmail.com>
2025-06-28 21:20:08 +0530

    Create the hello function for greeting

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,5 +2,9 @@
 pragma solidity ^0.8.19;
 contract HelloWorld {
     string public greeting = "Hello World!";
+
+    function hello() public view returns (string memory) {
+        return greeting;
+    }
 }
 
