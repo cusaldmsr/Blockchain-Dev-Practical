944f86c154d2d1a28e092b23a8b5d92e24979424
cusaldmsr <kusal2002d@Gmail.com>
2025-06-29 14:46:08 +0530

    Add constructor

--- contracts/Day2.sol
+++ contracts/Day2.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.30;
 
-contract Session02{
+contract Session02 {
     string message;
+
+    constructor(string memory _message) {
+        message = _message;
+    }
 
     function setMessage(string memory _newMsg) public {
         message = _newMsg;
     }
 
-    function getMessage() public view returns(string memory){
+    function getMessage() public view returns (string memory) {
         return message;
     }
 }
